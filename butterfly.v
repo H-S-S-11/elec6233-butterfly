6250869d6632657decc26650fac0d5bf4346592d
H.S.Snell <hss1g19@soton.ac.uk>
2023-04-28 14:30:53 +0100

    packing multipliers into one DSP block

--- butterfly.v
+++ butterfly.v
@@ -332,7 +332,7 @@ end
 // https://community.intel.com/t5/Intel-Quartus-Prime-Software/Specific-to-Logic-lock-chip-planner/td-p/710447
 
 // Result = dataa_0*datab_0 +/- dataa_1*datab_1
-/*
+/* Quartus IP multiplier block
 mult_add multiplier(
   .result(result),          //  result.result
   .dataa_0({ {8{re_w_pipeline[7] }}, re_w_pipeline}),  // Re(w) reg -sign extended to 16 bits (chipverify.com)
@@ -348,6 +348,7 @@ mult_add multiplier(
 );
 */
 
+// Result = dataa_0*datab_0 +/- dataa_1*datab_1
 mult_add_inferred multiplier(
   .result(result),          //  result.result
   .dataa_0({ {8{re_w_pipeline[7] }}, re_w_pipeline}),  // Re(w) reg -sign extended to 16 bits (chipverify.com)
@@ -358,18 +359,20 @@ mult_add_inferred multiplier(
   .ena0(load_coeff),    // load w
   .ena1(load_mult),     // load mult 0, 1
   .ena2(multiply) ,     // output reg enable
-  .aclr0(reset), .aclr1(reset), .aclr2(reset),  // Async active high reset
+  .aclr0(reset), .aclr1(reset),  // Async active high reset
   .clock0(clk), .clock1(clk), .clock2(clk)      // common clk
 );
 
 endmodule
 
+// https://www.intel.com/content/www/us/en/docs/programmable/683323/18-1/inferring-multipliers.html 
+// https://www.intel.com/content/www/us/en/docs/programmable/683082/22-1/inferring-multiply-accumulator-and-multiply.html 
 module mult_add_inferred(
   output reg [32:0] result,
   input wire [15:0] dataa_0, dataa_1, datab_0, datab_1,
   input wire clock0, clock1, clock2,
   input wire ena0, ena1, ena2,
-  input wire aclr0, aclr1, aclr2, addnsub1
+  input wire aclr0, aclr1, addnsub1
 );
 
 // Input registers
@@ -390,14 +393,36 @@ always @(posedge clock0, posedge aclr0) begin
   end
 end
 
+wire [15:0] negated_a_0, negated_a_1;
+assign negated_a_0 = addnsub1 ? dataa_0_reg : -dataa_0_reg;
+assign negated_a_1 = addnsub1 ? dataa_1_reg : -dataa_1_reg;
+
+macc_addsub_reg macc (
+ .result(result),
+ .dataa_0_reg(negated_a_0),
+ .dataa_1_reg(negated_a_1),
+ .datab_0_reg(datab_0_reg),
+ .datab_1_reg(datab_1_reg),
+ .addnsub1(addnsub1),
+ .clock2(clock2), .aclr1(aclr1), .ena2(ena2)
+);
+
+
+endmodule
+
+module macc_addsub_reg(
+  output reg [32:0] result,
+  input signed [15:0] dataa_0_reg, dataa_1_reg, datab_0_reg, datab_1_reg,
+  input wire addnsub1, clock2, aclr1, ena2
+);
+
 // Multipliers and output reg
-always @(posedge clock2, posedge aclr2) begin
-  if (aclr2) begin 
+always @(posedge clock2, posedge aclr1) begin
+  if (aclr1) begin 
     result <= 33'd0;
   end else begin
     if (ena2) begin
-      if (addnsub1) result <=  dataa_0_reg*datab_0_reg + dataa_1_reg*datab_1_reg;
-      else result <=  dataa_0_reg*datab_0_reg - dataa_1_reg*datab_1_reg;
+      result <= dataa_0_reg*datab_0_reg + dataa_1_reg*datab_1_reg;
     end
   end
 end
